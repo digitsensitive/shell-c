b5f859cd44604f6d3098c7041cb2db84944e1605
digitsensitive <digit.sensitivee@gmail.com>
2022-08-03 08:59:27 +0200

    resort functions in main

--- src/main.c
+++ src/main.c
@@ -25,31 +25,59 @@ char *builtin_str[] = {"cd", "help", "exit"};
 
 int (*builtin_func[])(char **) = {&lsh_cd, &lsh_help, &lsh_exit};
 
-int main(int argc, char *argv[]) {
-  // Load config files, if any
+int lsh_num_builtins() { return sizeof(builtin_str) / sizeof(char *); }
 
-  // Run command loop
-  lsh_loop();
-
-  // Perform any shutdown/cleanup
-
-  return 0;
+int lsh_cd(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
 }
 
-void lsh_loop(void) {
-  char *line;
-  char **args;
+int lsh_help(char **args) {
+  int i;
+  printf("Stephen Brennan's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args) { return 0; }
+
+int lsh_launch(char **args) {
+  pid_t pid;
+  pid_t wpid;
   int status;
 
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+  pid = fork();
 
-    free(line);
-    free(args);
-  } while (status);
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
 }
 
 char *lsh_read_line(void) {
@@ -124,61 +152,6 @@ char **lsh_split_line(char *line) {
   return tokens;
 }
 
-int lsh_launch(char **args) {
-  pid_t pid;
-  pid_t wpid;
-  int status;
-
-  pid = fork();
-
-  if (pid == 0) {
-    // Child process
-    if (execvp(args[0], args) == -1) {
-      perror("lsh");
-    }
-    exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    // Error forking
-    perror("lsh");
-  } else {
-    // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
-
-  return 1;
-}
-
-int lsh_num_builtins() { return sizeof(builtin_str) / sizeof(char *); }
-
-int lsh_cd(char **args) {
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
-}
-
-int lsh_help(char **args) {
-  int i;
-  printf("Stephen Brennan's LSH\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
-
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
-
-  printf("Use the man command for information on other programs.\n");
-  return 1;
-}
-
-int lsh_exit(char **args) { return 0; }
-
 int lsh_execute(char **args) {
   int i;
 
@@ -192,5 +165,33 @@ int lsh_execute(char **args) {
       return (*builtin_func[i])(args);
     }
   }
+
   return lsh_launch(args);
+}
+
+void lsh_loop(void) {
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+int main(int argc, char *argv[]) {
+  // Load config files, if any
+
+  // Run command loop
+  lsh_loop();
+
+  // Perform any shutdown/cleanup
+
+  return 0;
 }
